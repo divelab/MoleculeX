f3e51c37a4d355645fb574888d3cacace62df0c1
Meng Liu <604629@gmail.com>
2020-08-28 17:26:55 -0500

    Update train.sh

--- moleculekit/graph/scripts/train.sh
+++ moleculekit/graph/scripts/train.sh
@@ -5,4 +5,4 @@ An example script used to train model
 GPU=0
 
 echo "=====Train ml2 on qm8 (random split seed=122)====="
-CUDA_VISIBLE_DEVICES=${GPU} python ../src/main.py --save_dir=../trained_models/qm8/
+CUDA_VISIBLE_DEVICES=${GPU} python ../src/main.py --dataset=qm8 --save_dir=../trained_models/qm8/
